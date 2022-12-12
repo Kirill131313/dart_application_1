57767414a52c9bfd4d4d7a72f5923489f8ba5588
Kirill Pavlovich <kirill1387@gmail.com>
2022-12-12 17:10:47 +0200

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,26 @@
+var num = 10;
+var word = 'Hello';
+
+void main(List<String> arguments) {
+  print('Hello');
+  //print('world');
+  var point = 4.5;
+  point = point + 1;
+  point++;
+  print(point);
+
+  dynamic dynamicData = 5;
+  dynamicData = 'word';
+  dynamicData = false;
+  print(dynamicData);
+
+  var a = 15, b = 5;
+  print('${a + b}');
+  print('${a - b}');
+  print('${a * b}');
+  print('${a / b}');
+
+  if (5 != 5) {
+    print('hello');
+  }
+}
